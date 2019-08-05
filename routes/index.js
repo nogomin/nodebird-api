63df4a89af6db9721b696eedb7b6f9076dbb40c9
nogomin <tttime0@gmail.com>
2019-08-05 21:18:33 +0900

    JWT 사용량 제한 추가

--- routes/index.js
+++ routes/index.js
@@ -5,35 +5,35 @@ const { User, Domain } = require('../models');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    User.findOne({
-        where: { id: req.user && req.user.id || null},
-        include: { model: Domain },
+  User.findOne({
+    where: { id: req.user && req.user.id || null },
+    include: { model: Domain },
+  })
+    .then((user) => {
+      res.render('login', {
+        user,
+        loginError: req.flash('loginError'),
+        domains: user && user.domains,
+      });
     })
-        .then((user) => {
-            res.render('login', {
-                user,
-                loginError: req.flash('loginError'),
-                domains: user && user.domains,
-            });
-        })
-        .catch((error) => {
-            next(error);
-        });
+    .catch((error) => {
+      next(error);
+    });
 });
 
 router.post('/domain', (req, res, next) => {
-    Domain.create({
-        userId: req.user.id,
-        host: req.body.host,
-        type: req.body.type,
-        clientSecret: uuidv4(),
+  Domain.create({
+    userId: req.user.id,
+    host: req.body.host,
+    type: req.body.type,
+    clientSecret: uuidv4(),
+  })
+    .then(() => {
+      res.redirect('/');
     })
-        .then(() => {
-            res.redirect('/');
-        })
-        .catch((error) => {
-            next(error);
-        });
+    .catch((error) => {
+      next(error);
+    });
 });
 
 module.exports = router;
